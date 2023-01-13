0144e514f23295ecaa8b928ccf25f3c418c87a94
Krzysztof Czereczon <kczereczon@gmail.com>
2023-01-13 01:02:37 +0100

    made bloc working

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:letbudget/screens/home/home.dart';
+import 'package:letbudget/screens/home/sections/budget-section/budget-section.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (_) => HomeBloc(), child:  HomeView(),);
+    return MultiBlocProvider(providers: [
+      BlocProvider<HomeBloc>(create: (_) => HomeBloc()),
+      BlocProvider<BudgetSectionBloc>(create: (_) => BudgetSectionBloc()..add(BudgetSectionFetched())),
+    ], child: HomeView());
   }
+
+  const HomePage({Key? key}) : super(key: key);
 }
--- lib/screens/home/sections/budget-section/budget-section-view.dart
+++ lib/screens/home/sections/budget-section/budget-section-view.dart
@@ -10,45 +10,67 @@ class BudgetSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => BudgetSectionBloc()..add(BudgetSectionFetched()),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.indigo,
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/1.png'),
-                colorFilter: ColorFilter.mode(
-                    Theme.of(context).primaryColor, BlendMode.color)),
-            borderRadius: BorderRadius.all(Radius.circular(8))),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Shimmer.fromColors(
-
-                  baseColor: Theme.of(context).primaryColorDark,
-                  highlightColor: Colors.white,
-            enabled: true,
-                  child:
-                      Container(width: 200,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColorDark,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(8))))),
-                SizedBox(height: 5,),
-                const Text(
-                  "Your balance",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w200),
-                )
-              ])
-            ],
-          ),
-        ),
-      ),
-    );
+    return BlocBuilder<BudgetSectionBloc, BudgetSectionState>(
+        builder: (context, state) {
+          return Container(
+            decoration: BoxDecoration(
+                color: Colors.indigo,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('images/1.png'),
+                    colorFilter: ColorFilter.mode(
+                        Theme
+                            .of(context)
+                            .primaryColor, BlendMode.color)),
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    (<Widget>() {switch (state.status) {
+                      case BudgetSectionStatus.success:
+                        return Text(
+                          state.amount.toString(),
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        );
+                      case BudgetSectionStatus.initial:
+                        return Shimmer.fromColors(baseColor: Theme
+                            .of(context)
+                            .primaryColorDark,
+                            highlightColor: Colors.white,
+                            enabled: true,
+                            child: Container(width: 200, height: 35,
+                                decoration: BoxDecoration(
+                                    color: Theme
+                                        .of(context)
+                                        .primaryColorDark,
+                                    borderRadius:
+                                    BorderRadius.all(Radius.circular(8)))));
+                      case BudgetSectionStatus.failure:
+                        return const Text(
+                          "Error during fetching amount",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        );
+                    }})(),
+                    const SizedBox(height: 5,),
+                    const Text(
+                      "Your balance",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w200),
+                    )
+                  ])
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
