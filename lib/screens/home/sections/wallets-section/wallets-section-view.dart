a2e3d2c4f740309519b2430a0f013aa89af920b2
Krzysztof Czereczon <kczereczon@gmail.com>
2023-01-16 23:35:05 +0100

    added budgets with syncing

--- lib/screens/home/sections/wallets-section/wallets-section-view.dart
+++ lib/screens/home/sections/wallets-section/wallets-section-view.dart
@@ -4,6 +4,7 @@ import 'package:letbudget/screens/home/sections/sections.dart';
 import 'package:letbudget/screens/home/sections/wallets-section/components/components.dart';
 import 'package:letbudget/screens/home/sections/wallets-section/components/wallet/wallet_view_shimmer.dart';
 import 'package:letbudget/utils/converter.dart';
+import 'package:shimmer/shimmer.dart';
 
 import 'components/wallet/wallet_bloc.dart';
 
@@ -26,19 +27,31 @@ class WalletsSection extends StatelessWidget {
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
+                  children: [
+                    const Text(
                       "Your budgets",
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    Text(
-                      "2 Budgets",
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w300),
-                    )
+                    state is WalletsSectionLoaded
+                        ? Text(
+                            "${state.budgets.length} Budgets",
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w300),
+                          )
+                        : Shimmer.fromColors(
+                            baseColor: Theme.of(context).primaryColorDark,
+                            highlightColor: Colors.white,
+                            enabled: true,
+                            child: Container(
+                                width: 100,
+                                height: 15,
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).primaryColorDark,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8)))))
                   ],
                 ),
                 TextButton(
@@ -64,13 +77,14 @@ class WalletsSection extends StatelessWidget {
                     itemCount: state is WalletsSectionLoaded
                         ? state.budgets.length
                         : 5,
-                    itemBuilder: ((context, index) =>
-                        state is WalletsSectionLoaded
-                            ? Wallet(
-                                money: converter.formatValue(state.budgets[index].amount),
-                                image: Image.network(state.budgets[index].imageUrl),
-                                subcategory: state.budgets[index].subcategory)
-                            : const WalletShimmer()))),
+                    itemBuilder: ((context, index) => state
+                            is WalletsSectionLoaded
+                        ? Wallet(
+                            money: converter
+                                .formatValue(state.budgets[index].amount),
+                            image: Image.network(state.budgets[index].imageUrl),
+                            subcategory: state.budgets[index].subcategory)
+                        : const WalletShimmer()))),
           ],
         );
       },
